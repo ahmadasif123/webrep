a6d0553e81badce1e8d7b7f0fac180ca7260fce0
HP <HP@DESKTOP-HHKMTTK>
2024-05-26 01:57:45 +0500

    Add project files.

---
+++ HOPE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HOPE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
